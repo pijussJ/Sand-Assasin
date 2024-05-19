9ca21ccf247c148265277b59ef28e62b62784252
BigCoderDominykas <146240089+BigCoderDominykas@users.noreply.github.com>
2024-05-19 14:53:30 +0300

    Test

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public List<Transform> targets = new List<Transform>();
+
+    int currentIndex = 0;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+        NextDestination();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, agent.destination) < 1)
+        {
+            NextDestination();
+        }
+    }
+
+    private void NextDestination()
+    {
+        agent.destination = targets[currentIndex].position;
+
+        currentIndex++;
+        if (currentIndex >= targets.Count)
+        {
+            currentIndex = 0;
+        }
+    }
+}
